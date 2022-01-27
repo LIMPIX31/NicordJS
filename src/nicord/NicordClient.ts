b71b6810b099142c13d64b73b9372b975d4d0509
LIMPIX31 <limpix31@gmail.com>
2022-01-27 17:32:07 +0300

    Client setup

---
+++ src/nicord/NicordClient.ts
@@ -0,0 +1,6 @@
+import { NicordClient as INicordClient } from '../types/interfaces/NicordClient'
+import { Client } from 'discord.js'
+
+export class NicordClient extends Client implements INicordClient {
+
+}
